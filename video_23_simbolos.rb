947bfa71cf3b7960eb66e8bf3f6bf1e07553a80c
Joel Rojas <ejoelrojas9@gmail.com>
2022-01-25 18:23:41 -0500

    videos 23 y 24 de codigofacilito

---
+++ video_23_simbolos.rb
@@ -0,0 +1,25 @@
+#Simbolos
+
+#capitalize! coloca en mayucula la primera letra
+#cadena = "joel"
+#cadena.capitalize! 
+#puts cadena
+
+cadena = "Joel"
+cadena2 = "Joel"
+simbolo = :Joel
+simbolo2 = :Joel
+
+#cambian de id
+puts cadena.object_id #object_id muestra el id de la cadena independietemente de donde este ubicado
+puts cadena2.object_id
+
+#comparten el id
+puts simbolo.object_id
+puts simbolo2.object_id
+
+#Cuando utilizar los simbolos
+#1- Cuando no necesito modificar el string
+#2- Cuando no necesito los metodos del string 
+#3- Los simbolos se usan como nombres de cosas
+
